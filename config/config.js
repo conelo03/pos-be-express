cd616cc9170017868d7d85c1a763d94bf81f9b8b
Muhamad Ramadhan <ramadhan071thea@gmail.com>
2025-09-06 18:27:04 +0700

    setting env

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
+  }
+};
+
+module.exports = config[env];
